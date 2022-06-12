40c2ec8615813a9c8bca86a8c25d0f0cd2997b10
Jetty <jetty_park@gccompany.co.kr>
2022-06-12 19:52:30 +0900

    :sparkles: AppHeader 요소 생성

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/login">로그인</router-link> |
-    <router-link to="/signup">회원가입</router-link>
-  </nav>
-  <router-view></router-view>
+  <div>
+    <app-header></app-header>
+    <router-view></router-view>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import AppHeader from '@/components/common/AppHeader.vue';
 
-nav {
-  padding: 30px;
-}
+export default {
+  components: {
+    AppHeader,
+  },
+};
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
