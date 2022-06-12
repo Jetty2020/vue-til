ecb3edb93ae1c5b9c2b4a36e9e55edbb11c2872f
Jetty <jetty_park@gccompany.co.kr>
2022-06-12 23:32:51 +0900

    :sparkles: LoginForm 에러 처리

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -25,14 +25,19 @@ export default {
   },
   methods: {
     async submitForm() {
-      const userData = {
-        username: this.username,
-        password: this.password,
-      };
-      const { data } = await loginUser(userData);
-      console.log(data);
-      this.logMessage = `${data.user.username} 님 환영합니다.`;
-      this.initForm();
+      try {
+        const userData = {
+          username: this.username,
+          password: this.password,
+        };
+        const { data } = await loginUser(userData);
+        console.log(data);
+        this.logMessage = `${data.user.username} 님 환영합니다.`;
+      } catch (error) {
+        this.logMessage = error.response.data;
+      } finally {
+        this.initForm();
+      }
     },
     initForm() {
       this.username = '';
