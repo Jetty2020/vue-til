82b4b2f3e59d9d169199b1c742538489b863b0e2
Jetty <jetty_park@gccompany.co.kr>
2022-06-12 18:56:52 +0900

    :sparkles: 로그인, 회원가입 페이지 생성 및 라우팅 연결

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/login">로그인</router-link> |
+    <router-link to="/signup">회원가입</router-link>
   </nav>
   <router-view />
 </template>
