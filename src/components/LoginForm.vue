956f3e50634a296c3f7eb9943dc123f7a40c9617
Jetty <jetty_park@gccompany.co.kr>
2022-06-12 23:32:15 +0900

    :sparkles: LoginForm 요소 생성

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="text" v-model="password" />
+    </div>
+    <button type="submit">로그인</button>
+    <p>{{ logMessage }}</p>
+  </form>
+</template>
+
+<script>
+import { loginUser } from '@/api';
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      logMessage: '',
+    };
+  },
+  methods: {
+    async submitForm() {
+      const userData = {
+        username: this.username,
+        password: this.password,
+      };
+      const { data } = await loginUser(userData);
+      console.log(data);
+      this.logMessage = `${data.user.username} 님 환영합니다.`;
+      this.initForm();
+    },
+    initForm() {
+      this.username = '';
+      this.password = '';
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/SignupPage.vue
+++ src/views/SignupPage.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h1>회원 가입 페이지</h1>
     <signup-form></signup-form>
   </div>
 </template>
