cd4ac434105894a62cd418d427155e967d58f8b8
Jetty <jetty_park@gccompany.co.kr>
2022-06-12 22:58:34 +0900

    :recycle: API baseURL 공통화

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,15 @@
 import axios from 'axios';
 
+const instance = axios.create({
+  baseURL: 'http://localhost:3000/',
+});
+
 function registerUser(userData) {
-  const url = 'http://localhost:3000/signup';
-  return axios.post(url, userData);
+  return instance.post('signup', userData);
 }
 
-export { registerUser };
+function loginUser(userData) {
+  return instance.post('login', userData);
+}
+
+export { registerUser, loginUser };
