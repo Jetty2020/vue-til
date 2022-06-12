03480a0f78f5bf3847f42b796791163ef0172bc2
Jetty <jetty_park@gccompany.co.kr>
2022-06-12 23:03:50 +0900

    :recycle: baseURL을 환경변수로 관리

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: process.env.VUE_APP_API_URL,
 });
 
 function registerUser(userData) {
