5876a959b2e56c763795823d9e0a885c5689425e
Jetty <jetty_park@gccompany.co.kr>
2022-06-14 20:10:11 +0900

    :sparkles: Add 버튼 생성

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class="main list-container contents">
-    <h1 class="page-header">Today I Learned</h1>
-    <loading-spinner v-if="isLoading"></loading-spinner>
-    <ul v-else>
-      <post-list-item
-        v-for="postItem in postItems"
-        :key="postItem._id"
-        :postItem="postItem"
-      ></post-list-item>
-    </ul>
+  <div>
+    <div class="main list-container contents">
+      <h1 class="page-header">Today I Learned</h1>
+      <loading-spinner v-if="isLoading"></loading-spinner>
+      <ul v-else>
+        <post-list-item
+          v-for="postItem in postItems"
+          :key="postItem._id"
+          :postItem="postItem"
+        ></post-list-item>
+      </ul>
+    </div>
+    <router-link to="/add" class="create-button">
+      <i class="ion-md-add"></i>
+    </router-link>
   </div>
 </template>
 
