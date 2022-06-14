27c3bcad223f305e8d592f0d75f68fb7789b6204
Jetty <jetty_park@gccompany.co.kr>
2022-06-14 19:33:30 +0900

    :recycle: PostItem 요소 추상화

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -2,21 +2,27 @@
   <div class="main list-container contents">
     <h1 class="page-header">Today I Learned</h1>
     <ul>
-      <li v-for="postItem in postItems" :key="postItem._id">
-        {{ postItem.title }}
-      </li>
+      <post-list-item
+        v-for="postItem in postItems"
+        :key="postItem._id"
+        :postItem="postItem"
+      ></post-list-item>
     </ul>
   </div>
 </template>
 
 <script>
 import { fetchPosts } from '@/api';
+import PostListItem from '../components/posts/PostListItem.vue';
 
 export default {
   data() {
     return {
       postItems: [],
     };
+  },
+  components: {
+    PostListItem,
   },
   methods: {
     async fetchData() {
