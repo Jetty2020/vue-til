4d62fb5885d8ceaf991c065e802e6b6637e0ed43
Jetty <jetty_park@gccompany.co.kr>
2022-06-18 17:53:02 +0900

    :sparkles: PostEditPage 생성

---
+++ src/components/posts/PostEditForm.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="contents">
+        <h1 class="page-header">Edit Post</h1>
+        <div class="form-wrapper">
+            <form class="form" @submit.prevent="submitForm">
+                <div>
+                    <label for="title">Title:</label>
+                    <input id="title" type="text" v-model="title" />
+                </div>
+                <div>
+                    <label for="contents">Contents:</label>
+                    <textarea
+                        id="contents"
+                        type="text"
+                        rows="5"
+                        v-model="contents"
+                    />
+                    <p
+                        v-show="!isContentsValid"
+                        class="validation-text warning"
+                    >
+                        Contents must be less than 250
+                    </p>
+                </div>
+                <button type="submit" class="btn">Edit</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: '',
+            contents: '',
+            logMessage: '',
+        };
+    },
+    computed: {
+        isContentsValid() {
+            return this.contents.length <= 250;
+        },
+    },
+    methods: {
+        submitForm() {},
+    },
+};
+</script>
+
+<style scoped>
+.form-wrapper .form {
+    width: 100%;
+}
+.btn {
+    color: #fff;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,44 +1,49 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import {createRouter, createWebHistory} from 'vue-router';
 
 const routes = [
-  {
-    path: '/',
-    redirect: '/login',
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: () => import('@/views/LoginPage.vue'),
-  },
-  {
-    path: '/signup',
-    name: 'signup',
-    component: () => import('@/views/SignupPage.vue'),
-  },
-  {
-    path: '/main',
-    name: 'main',
-    component: () => import('@/views/MainPage.vue'),
-  },
-  {
-    path: '/add',
-    name: 'add',
-    component: () => import('@/views/PostAddPage.vue'),
-  },
-  {
-    path: '/404',
-    name: 'NotFound',
-    component: () => import('@/views/NotFoundPage.vue'),
-  },
-  {
-    path: '/:pathMatch(.*)*',
-    redirect: '/404',
-  },
+    {
+        path: '/',
+        redirect: '/login',
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: () => import('@/views/LoginPage.vue'),
+    },
+    {
+        path: '/signup',
+        name: 'signup',
+        component: () => import('@/views/SignupPage.vue'),
+    },
+    {
+        path: '/main',
+        name: 'main',
+        component: () => import('@/views/MainPage.vue'),
+    },
+    {
+        path: '/add',
+        name: 'add',
+        component: () => import('@/views/PostAddPage.vue'),
+    },
+    {
+        path: '/post/:id',
+        name: 'edit',
+        component: () => import('@/views/PostEditPage.vue'),
+    },
+    {
+        path: '/404',
+        name: 'NotFound',
+        component: () => import('@/views/NotFoundPage.vue'),
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        redirect: '/404',
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
 export default router;
