65cb60ba0883fb9bdd2eb419758a517023fc9a34
Jetty <jetty_park@gccompany.co.kr>
2022-06-19 15:04:54 +0900

    :sparkles: 로고 이동 링크를 로그인 상태에 따라 분기 처리

--- src/components/common/AppHeader.vue
+++ src/components/common/AppHeader.vue
@@ -1,82 +1,89 @@
 <template>
-  <header>
-    <div>
-      <router-link to="/" class="logo">
-        TIL
-        <span v-if="isUserLogin">by {{ $store.state.username }}</span>
-      </router-link>
-    </div>
-    <div class="navigations">
-      <!-- 1 -->
-      <template v-if="isUserLogin">
-        <a href="javascript:;" @click="logoutUser" class="logout-button">
-          Logout
-        </a>
-      </template>
-      <!-- 2 -->
-      <template v-else>
-        <router-link to="/login">로그인</router-link>
-        <router-link to="/signup">회원가입</router-link>
-      </template>
-    </div>
-  </header>
+    <header>
+        <div>
+            <router-link :to="logoLink" class="logo">
+                TIL
+                <span v-if="isUserLogin">by {{ $store.state.username }}</span>
+            </router-link>
+        </div>
+        <div class="navigations">
+            <!-- 1 -->
+            <template v-if="isUserLogin">
+                <a
+                    href="javascript:;"
+                    @click="logoutUser"
+                    class="logout-button"
+                >
+                    Logout
+                </a>
+            </template>
+            <!-- 2 -->
+            <template v-else>
+                <router-link to="/login">로그인</router-link>
+                <router-link to="/signup">회원가입</router-link>
+            </template>
+        </div>
+    </header>
 </template>
 
 <script>
 export default {
-  computed: {
-    isUserLogin() {
-      return this.$store.getters.isLogin;
+    computed: {
+        isUserLogin() {
+            return this.$store.getters.isLogin;
+        },
+        logoLink() {
+            return this.$store.getters.isLogin ? '/main' : '/login';
+        },
     },
-  },
-  methods: {
-    logoutUser() {
-      this.$store.commit('clearUsername');
-      this.$router.push('/login');
+    methods: {
+        logoutUser() {
+            this.$store.commit('clearUsername');
+            this.$router.push('/login');
+        },
     },
-  },
 };
 </script>
 
 <style scoped>
 .username {
-  color: white;
+    color: white;
 }
 header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px 20px;
-  background-color: #927dfc;
-  z-index: 2;
-  box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.05);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+    background-color: #927dfc;
+    z-index: 2;
+    box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.05);
 }
 a {
-  color: #dedede;
-  font-size: 18px;
+    color: #dedede;
+    font-size: 18px;
 }
 a.logo {
-  font-size: 30px;
-  font-weight: 900;
-  color: white;
+    font-size: 30px;
+    font-weight: 900;
+    color: white;
 }
 .logo > span {
-  font-size: 14px;
-  font-weight: normal;
+    font-size: 14px;
+    font-weight: normal;
 }
 .navigations a {
-  margin-left: 10px;
+    margin-left: 10px;
 }
 .fixed {
-  position: fixed;
-  top: 0;
-  width: 100%;
+    position: fixed;
+    top: 0;
+    width: 100%;
 }
 .logout-button {
-  font-size: 14px;
+    font-size: 14px;
 }
 a.router-link-exact-active {
-  color: white;
-  font-weight: bold;
+    color: white;
+    font-weight: bold;
 }
 </style>
