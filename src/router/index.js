487709831c12cca29ebdf726051a7c76fe60708a
Jetty <jetty_park@gccompany.co.kr>
2022-06-19 15:09:16 +0900

    :sparkles: 로그인이 필요한 페이지에 네비게이션 가드 구현

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import {createRouter, createWebHistory} from 'vue-router';
 
+import store from '@/store';
+
 const routes = [
     {
         path: '/',
@@ -19,16 +21,25 @@ const routes = [
         path: '/main',
         name: 'main',
         component: () => import('@/views/MainPage.vue'),
+        meta: {
+            auth: true,
+        },
     },
     {
         path: '/add',
         name: 'add',
         component: () => import('@/views/PostAddPage.vue'),
+        meta: {
+            auth: true,
+        },
     },
     {
         path: '/post/:id',
         name: 'edit',
         component: () => import('@/views/PostEditPage.vue'),
+        meta: {
+            auth: true,
+        },
     },
     {
         path: '/404',
@@ -44,6 +55,14 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes,
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.auth && !store.getters.isLogin) {
+        next('/login');
+        return;
+    }
+    next();
 });
 
 export default router;
